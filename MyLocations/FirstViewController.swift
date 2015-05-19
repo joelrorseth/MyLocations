a54fc39d0704287522e9785d88dfdf1d2b552628
Joel <Joel@joels-imac.gateway.2wire.net>
2015-05-18 20:37:10 -0400

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Joel on 2015-05-18.
+//  Copyright (c) 2015 Joel Rorseth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
